8b8d405794c6b19f56998e6bd063e9b022971f75
MatthewBeast56 <62149740+MatthewBeast56@users.noreply.github.com>
2020-05-13 11:24:45 -0400

    Update gamehider.js

--- bookmarklets/gamehider.js
+++ bookmarklets/gamehider.js
@@ -9,7 +9,7 @@ var SHEEP_2048;
     }
     var wrapper=document.createElement("div"),
     frame=document.createElement("iframe");
-    frame.src="https://matthewbeast56.github.io";
+    frame.src="https://matthewbeast56.github.io/index.html/";
     frame.width=333; // additional 30px to hide scrollbars
     frame.height=461;
     frame.scrolling="no";
