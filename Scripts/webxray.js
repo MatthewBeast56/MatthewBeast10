29abdc97e422dc6243cf67c768e3ffca24673fb5
MatthewBeast56 <62149740+MatthewBeast56@users.noreply.github.com>
2021-02-15 20:44:21 -0500

    Update webxray.js

--- Scripts/webxray.js
+++ Scripts/webxray.js
@@ -186,7 +186,7 @@ var JSON = {
 };
 
 var defaultLang = "en-US",
-	defaultURI = "https://x-ray-goggles.mouse.org" + "/" + defaultLang;
+	defaultURI = "https://matthewbeast56.github.io/MatthewBeast10/Scripts/" + "/" + defaultLang;
 
 var webxrayScript = document.querySelector(".webxray"),
 	baseURI = baseURI = webxrayScript.getAttribute("data-baseuri") || defaultURI,
@@ -10046,9 +10046,9 @@ var Webxray = (function() {
     baseURI: "",
     cssURL: "webxray.css",
     preferencesURL: "preferences.html",
-    easyRemixDialogURL: "https://x-ray-goggles.mouse.org/" + xray.lang + "/easy-remix-dialog/index.html",
-    uprootDialogURL: "https://x-ray-goggles.mouse.org/" + xray.lang + "/uproot-dialog.html",
-    helpDialogURL: "https://x-ray-goggles.mouse.org/" + xray.lang + "/help.html",
+    easyRemixDialogURL: "https://matthewbeast56.github.io/MatthewBeast10/Scripts/" + xray.lang + "/easy-remix-dialog/index.html",
+    uprootDialogURL: "https://matthewbeast56.github.io/MatthewBeast10/Scripts/" + xray.lang + "/uproot-dialog.html",
+    helpDialogURL: "https://matthewbeast56.github.io/MatthewBeast10/Scripts/" + xray.lang + "/help.html",
     hackpubInjectionURL: "published-hack/injector.js",
     pluginURLs: [],
     hackpubURL: "",
